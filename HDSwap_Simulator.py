d88265387cce40223ff870ec18fce0d85f3b8664
stefanopaesani <stefano.paesani@bristol.ac.uk>
2020-11-06 18:40:37 +0000

    Added Simulator file

--- HDSwap_Simulator.py
+++ HDSwap_Simulator.py
@@ -4,7 +4,6 @@
 Stefano Paesani, Nov. 2020
 """
 
-
 from GaussianTransformations import *
 import numpy as np
 
@@ -13,40 +12,12 @@ import numpy as np
 ##########################################################################################
 if __name__=="__main__":
 
-
-
-
     dim =
     s_par = 0.2
     num_modes = 2
     cov0 = np.identity(2*num_modes)
     cov = cov0
 
-
-### Tests with Stefano's evolution method
-    cov_new = cov0
-    cov_new = symplectic_evolution(cov_new, get_tms_sym(s_par, 0, 0, 1, num_modes))
-
-
-##### tests with strawberryfield
-
-    eng = sf.Engine('gaussian')
-
-    ##### test TMS
-    prog2 = sf.Program(num_modes)
-    with prog2.context as q:
-        S2 = S2gate(s_par)
-        S2 | (q[0], q[1])
-    state2 = eng.run(prog2).state
-    # print(state2.means())
-    print(state2.cov())
-
-
-    mapped_state =  map_into_StrawberryFields(np.zeros(num_modes), cov_new, num_modes)
-    # print(mapped_state.means())
-    print(mapped_state.cov())
-
-    print(mapped_state == state2)
 
 
 
